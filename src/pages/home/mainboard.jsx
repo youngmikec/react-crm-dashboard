245a96d7332c7cb4f0ed700d013d2218af72e7a4
youngmikec <michaelozor15@gmail.com>
2022-05-18 10:32:05 +0100

    reviwed and modified the mainboard component

--- src/pages/home/mainboard.jsx
+++ src/pages/home/mainboard.jsx
@@ -5,31 +5,34 @@ const Mainboard = () => {
     const {account,pie,region} = images
   return (
       <>
-    <h3>Sales overview</h3>
-    <div className='d-flex'>
-        <div className='p-5'>
-            <p>Total sales</p>
-            <img className ='w-100' src={account} alt="" />
-            <div className='d-flex flex-row justify-content-between'>
-                <div>
-                <h4>Monthly sales</h4>
-                <strong>129,543</strong>
-                </div>
-                <div>
-                    <h4>Sales revenue</h4>
-                    <strong>20%</strong>
+        <div className='container pl-3'>
+            <h3>Sales overview</h3>
+        </div>
+
+        <div className='d-flex'>
+            <div className='p-5'>
+                <p>Total sales</p>
+                <img className ='w-100' src={account} alt="" />
+                <div className='d-flex flex-row justify-content-between'>
+                    <div>
+                    <h4>Monthly sales</h4>
+                    <strong>129,543</strong>
+                    </div>
+                    <div>
+                        <h4>Sales revenue</h4>
+                        <strong>20%</strong>
+                    </div>
                 </div>
             </div>
+            <div>
+            <p>pie distribution</p>
+            <img className='w-50 h-25 mb-2'src={pie} alt="" />
+            <div>
+            <p>by region</p>
+            <img className='w-100 h-50' src={region} alt="" />
+            </div>
+            </div>
         </div>
-        <div>
-        <p>pie distribution</p>
-        <img className='w-50 h-25 mb-2'src={pie} alt="" />
-        <div>
-        <p>by region</p>
-        <img className='w-100 h-50' src={region} alt="" />
-        </div>
-        </div>
-    </div>
       </>
   )
 }
